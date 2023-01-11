70df5bea2327e8bc9ea4b3b097dbc3bab36050d5
Caleb Waymeyer <cjwaymeyer@gmail.com>
2023-01-11 16:36:32 -0500

    Initial commit

---
+++ test/arcturus.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Arcturus from '../lib/arcturus-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/arcturus-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Arcturus.ArcturusStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
